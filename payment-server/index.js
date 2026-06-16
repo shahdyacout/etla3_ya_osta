5950119e00745dd8efea72ba35cd7e36e0bd33cf
shahdyacout <shahdyacout4@gmail.com>
2026-06-16 11:33:50 +0300

    Update index.js

--- payment-server/index.js
+++ payment-server/index.js
@@ -82,5 +82,5 @@ app.post('/webhook', (req, res) => {
 
 app.get('/', (req, res) => res.send('Masar Payment Server Running ✅'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
